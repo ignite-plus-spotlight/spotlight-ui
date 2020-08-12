23421e0f8b6f1813971ca3571f8ef002126bc637
rithikaprakash <60447231+rithikaprakash@users.noreply.github.com>
2020-08-12 22:06:04 +0530

    Poll axios done

--- src/pages/poll/Poll.js
+++ src/pages/poll/Poll.js
@@ -1,19 +1,18 @@
-import React, { useState } from "react";
-import Layout from '../layout/Layout'
-import {
-    Typography,
-    Paper,
-    Grid,
-    Button,
-    CssBaseline,
-    FormControlLabel,
-  } from '@material-ui/core';
-  import { Form, Field } from 'react-final-form'
+import React,{useState,useEffect}from 'react';
+import axios from 'axios';
+import { Form, Field } from 'react-final-form'
 import { Checkbox } from 'final-form-material-ui';
 import TextField from '@material-ui/core/TextField';
+import {
+  Typography,
+  Paper,
+  Grid,
+  Button,
+  CssBaseline,
+  FormControlLabel,
+} from '@material-ui/core';
+import Layout from '../layout/Layout';
 import { makeStyles } from '@material-ui/core/styles';
-import Fab from '@material-ui/core/Fab';
-import AddIcon from '@material-ui/icons/Add';
 
 const useStyles = makeStyles((theme) => ({
     container: {
@@ -27,133 +26,173 @@ const useStyles = makeStyles((theme) => ({
     },
   }));
 
-const onSubmit = async values => {
+
+
+export default function Poll() {
+
+    const classes = useStyles();
+
+    const [data,setData]=useState({
+      pollName:"",
+      pollId:"",
+      description:"",
+      pollStart:"",
+      pollEnd:"",
+      nomStart:"",
+      nomEnd:""
+    })
+
+    function handle(e) {
+      const newdata={...data}
+      newdata[e.target.id]=e.target.value
+      setData(newdata)
+      console.log(data);
+    }
+    const url=`http://localhost:8081/postmultiple`
+   
+   
+   
+  //   const onSubmit = e => {
+  //     // e.preventDefault()
+  //     axios.post(url,data)
+  //     .then(res=>{
+  //       console.log(res.data)
+  //     })
+  // };
+
+  const onSubmit = e => {
+    // e.preventDefault()
+    axios.post(url,data,{
+      headers: {
+        'Content-Type': 'application/json',
+    }
+    })
+    
 };
+  
 
-function Poll() {
-  const [inputList, setInputList] = useState([{ firstName: "", lastName: "" }]);
-  const classes = useStyles();
+    
+    
+ 
+    
+    return (
+        <Layout>
+          <div style={{ padding: 16, margin: 'auto', maxWidth: 600 }}>
+             <CssBaseline />
+             <Typography variant="h4" align="center" component="h1" gutterBottom>
+                 POLL
+             </Typography>
+             <Typography variant="h5" align="center" component="h2" gutterBottom>
+                   Enter the Details
+             </Typography>
 
-  // handle input change
-  const handleInputChange = (e, index) => {
-    const { name, value } = e.target;
-    const list = [...inputList];
-    list[index][name] = value;
-    setInputList(list);
-  };
-
-
-  // handle click event of the Add button
-  const handleAddClick = () => {
-    setInputList([...inputList, { Description: ""}]);
-  };
-
-
-//  const url=`http://localhost:8081/${pollname}/${description}/${nomstart}/${nomend}/${pollstart}/${pollend}/${pollid}`
-
-  return (
-    <Layout>
-         <div style={{ padding: 16, margin: 'auto', maxWidth: 600 }}>
-      <CssBaseline />
-      <Typography variant="h4" align="center" component="h1" gutterBottom>
-         POLL
-      </Typography>
-      <Typography variant="h5" align="center" component="h2" gutterBottom>
-        Enter the Details
-      </Typography>
-      <Form
+             <Form
         onSubmit={onSubmit}
         render={({ handleSubmit, reset, submitting, pristine, values }) => (
-          <form onSubmit={handleSubmit} noValidate>
-            <Paper style={{ padding: 16 }}>
-              <Grid container alignItems="flex-start" spacing={2}>
-                <Grid item xs={12}>
-                  <Field
-                    fullWidth
-                    required
-                    color="secondary"
-                    name="Poll Name"
-                    component={TextField}
-                    type="text"
-                    label="Poll Name"
-                  />
-                </Grid>
-                
-      {inputList.map((x, i) => {
-        return (
-        <Grid item xs={12}>
-        <Grid item xs={12}>
-              <Field
-                    name="Poll Description"
-                    color="secondary"
-                    fullWidth
-                    component={TextField}
-                    type="text"
-                    label="Poll Description"
-                    value={x.firstName}
-                    onChange={e => handleInputChange(e, i)}
-                  />
- </Grid>
-            <Grid item xs={2}>
-              {/* {inputList.length - 1 === i && <button onClick={handleAddClick}>Add</button>} */}
-              {inputList.length - 1 === i && 
-              <Fab color="secondary" size="small" aria-label="add" onClick={handleAddClick}>
-             <AddIcon />
-            </Fab>}
-              </Grid>
-          
-         </Grid>
-        );
-       })} 
-       <Grid item xs={12}>
-                 <TextField
-                    id="datetime-local"
-                    label="Nomination Start Date"
-                    type="datetime-local"
-                    color="secondary"
-                    // defaultValue="2017-05-24T10:30"
-                    className={classes.textField}
-                    InputLabelProps={{
-                     shrink: true,
-                    }}
-                 />
-                   <TextField
-                    id="datetime-local"
-                    label="Nomination End Date"
-                    type="datetime-local"
-                    color="secondary"
-                    // defaultValue="2017-05-24T10:30"
-                    className={classes.textField}
-                    InputLabelProps={{
-                     shrink: true,
-                    }}
-                 />
-                 </Grid>
+          <form onSubmit={handleSubmit} noValidate >
+
+                   <Paper style={{ padding: 16 }}>
+                     <Grid container alignItems="flex-start" spacing={2}>
+
+                     <Grid item xs={12}>
+                          <Field
+                            fullWidth
+                            required
+                            id="pollId"
+                            name="Poll ID"
+                            component={TextField}
+                            type="text"
+                            label="Poll ID"
+                            input onChange={(e)=>handle(e)}
+                             value={data.pollId}
+                          />
+                      </Grid>
+
+                       <Grid item xs={12}>
+                          <Field
+                            fullWidth
+                            required
+                            id="pollName"
+                            name="Poll Name"
+                            component={TextField}
+                            type="text"
+                            label="Poll Name"
+                            input onChange={(e)=>handle(e)}
+                            value={data.pollName}
+                          />
+                      </Grid>
+
+                      <Grid item xs={12}>
+                        <Field
+                          name="Poll Description"
+                          fullWidth
+                          id="description"
+                          component={TextField}
+                          type="text"
+                          label="Poll Description"
+                          input onChange={(e)=>handle(e)}
+                          value={data.description}
+  
+                        />
+                      </Grid>
+               
+                      <Grid item xs={12}>
+
+                        <TextField
+                            id="nomStart"
+                            label="Nomination Start Date"
+                            type="datetime-local"
+                            input onChange={(e)=>handle(e)}
+                            value={data.nominationStartDate}
+                            // className={classes.textField}
+                            InputLabelProps={{
+                            shrink: true,
+                            }}
+                        />
+
+                        <TextField
+                          id="nomEnd"
+                          label="Nomination End Date"
+                          type="datetime-local"
+                          input onChange={(e)=>handle(e)}
+                          value={data.nominationEndDate}
+                         
+                          // className={classes.textField}
+                          InputLabelProps={{
+                          shrink: true,
+                          }}
+                        />
+                      </Grid>
+
                  <Grid item xs={12}>
                  <TextField
-                    id="datetime-local"
+                    id="pollStart"
                     label="Poll Start Date"
                     type="datetime-local"
-                    color="secondary"
                     // defaultValue="2017-05-24T10:30"
-                    className={classes.textField}
+                    input onChange={(e)=>handle(e)}
+                    value={data.pollStartDate}
+                    
+                    // className={classes.textField}
                     InputLabelProps={{
                      shrink: true,
                     }}
                  />
                    <TextField
-                    id="datetime-local"
+                    id="pollEnd"
                     label="Poll End Date"
                     type="datetime-local"
-                    color="secondary"
                     // defaultValue="2017-05-24T10:30"
-                    className={classes.textField}
+                    input onChange={(e)=>handle(e)}
+                    value={data.pollEndDate}
+                    
+                    // className={classes.textField}
                     InputLabelProps={{
                      shrink: true,
                     }}
                  />
                  </Grid>
-               {/* </form> */}
+               
                 <Grid item xs={12}>
                   <FormControlLabel
                     label="Confirm"
@@ -167,7 +206,11 @@ function Poll() {
                   />
                 </Grid>
                 <Grid item style={{ marginTop: 16 }}>
-                  <Button
+                  
+                   {/* <Button onClick={(e)=>onSubmit(e)} color="secondary">
+       Send 
+       </Button> */}
+       <Button
                     variant="contained"
                     color="secondary"
                     type="submit"
@@ -175,16 +218,16 @@ function Poll() {
                   >
                     Post
                   </Button>
+
                 </Grid>
+                
               </Grid>
             </Paper>
-          </form>
+            
+            </form>
         )}
       />
-      
-      </div>
-    </Layout>
-  );
+    </div>
+        </Layout>
+    )
 }
-
-export default Poll;
